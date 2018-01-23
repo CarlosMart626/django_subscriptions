d8ec3a0b6658331fea185a7c6c6c7c7e37d05b0c
Carlos Martinez <carlosmart626@gmail.com>
2018-01-23 07:36:41 -0500

    Update example

--- django_subscriptions/schema.py
+++ django_subscriptions/schema.py
@@ -43,12 +43,19 @@ class Subscription(graphene.ObjectType):
         gp_name = 'gqp.{0}-updated.{1}'.format(
             str.lower(product_object.__class__.__name__), product_object.pk)
         print("gp_name", gp_name)
-        # print("info", dir(info))
+        print("info", info.context.__dict__)
+        print("session", info.context.channel_session.__dict__)
         # context = info.return_type
-        # print("context", dir(context), context)
-        # Group(gp_name).add(info.context.reply_channel)
-        # info.context.channel_session['Groups'] = ','.join(
-        #     (gp_name, info.context.channel_session['Groups']))
+        print("channel_layer", info.context.reply_channel.channel_layer.__dict__)
+        Group(gp_name).add(info.context.reply_channel)
+        channel_session = info.context.channel_session
+        
+        all_channels = [
+            channel for channel in info.context.channel_layer.group_channels(gp_name)]
+        print("all_channels", all_channels)
+        info.context.channel_session.update(
+            {'Groups': ','.join((gp_name, info.context.channel_session.get('Groups', '')))})
+        print("channel_session", channel_session.__dict__)
         return Observable.from_iterable(iter([product_object]))
 
 
--- products/models.py
+++ products/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from channels import Group
 
 
 class Product(models.Model):
@@ -6,3 +9,14 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+@receiver(post_save, sender=Product)
+def send_update(sender, instance, created, *args, **kwargs):
+    uuid = str(instance.id)
+    if created:
+        Group("gqp.product-add").send({'added': True})
+        return
+    print("Message sent to channel", 'gqp.product-updated.{0}'.format(uuid))
+    Group('gqp.product-updated.{0}'.format(uuid))\
+        .send({'text': "Updated"})
